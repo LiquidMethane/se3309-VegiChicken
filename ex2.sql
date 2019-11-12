4a9fe091477ac6bcba1d7173ba2610e677096d83
rlim23 <46493295+rlim23@users.noreply.github.com>
2019-11-12 09:56:10 -0500

    create ex2.sql

---
+++ ex2.sql
@@ -0,0 +1,22 @@
+/* Database Schema */ 
+
+/* User Table */ 
+
+/* UserFavouritePart Table */ 
+
+/* Build Table */ 
+
+/* BuildPart Table */ 
+
+/* Part Table */ 
+
+/* Store Table */ 
+
+/* Inventory Table */ 
+
+/* CPU Table */ 
+
+/* Cooler Table */ 
+
+/* Memory Table */ 
+

9e7fec018e48bdae4fa0bb52f24d62ab41e9fe01
rlim23 <46493295+rlim23@users.noreply.github.com>
2019-11-19 12:25:17 -0500

    added query for returning lower than average prices for a part

--- assignment3/sql/queries.sql
+++ assignment3/sql/queries.sql
@@ -1,3 +1,6 @@
+/* Six Queries */ 
+
+/* Find a store that has the all of the parts for a certain build and the cheapest total price of those parts */
 select storeNo, sum(price) as total
 from 
 	(select storeNo, partNo, price
@@ -8,3 +11,14 @@ group by storeNo
 having count(partNo) = 8
 order by total asc
 limit 1;
+
+/*  Return all of the latest inventory prices that are lower than the average price for a certain part with no. 10000*/ 
+SELECT storeNo, partNo, price 
+FROM Inventory as i1
+WHERE partNo = 10000 
+AND price < 
+	(SELECT ROUND(AVG(price), 2)
+	FROM Inventory as i, Part as p
+	WHERE i.partNo = p.partNo AND i.partNo = 10000)
+AND inventoryDate = (SELECT MAX(inventoryDate) FROM Inventory as i2 WHERE i2.storeNo = i1.storeNo AND i2.partNo = i1.partNo )
+ORDER BY price; 

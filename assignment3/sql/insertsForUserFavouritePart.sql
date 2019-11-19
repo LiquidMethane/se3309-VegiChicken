74b50d7035f59b82ff54195c303f0ba80dcfa05b
rlim23 <46493295+rlim23@users.noreply.github.com>
2019-11-19 11:55:10 -0500

    updates to insert queries to avoid duplicate inserts

--- assignment3/sql/insertsForUserFavouritePart.sql
+++ assignment3/sql/insertsForUserFavouritePart.sql
@@ -4,18 +4,19 @@
 INSERT INTO UserFavouritePart
 VALUES ('1', 10040);
 
-/* Insert any favourite parts for a user that are Memory and where an inventory with a price less than $100 exists for that part  */
+/* Insert any favourite parts for a user with id 1 that are Memory and where an inventory with a price less than $100 exists for that part  */
 INSERT INTO UserFavouritePart
-SELECT '1', m.partNo
-FROM Memory as m
+SELECT 1, m.partNo
+FROM pcMemory as m
 WHERE EXISTS(SELECT * FROM Inventory as i 
-			 WHERE m.partNo = i.partNo AND price < 100.00) ;
+			 WHERE m.partNo = i.partNo AND price > 0.00 AND price < 100.00) 
+AND NOT EXISTS(SELECT partNo FROM UserFavouritePart as ufp WHERE userId = 1 AND ufp.partNo = m.partNo);
 
 /* Insert Favourite parts for a user that are in any of the user's builds and aren't already in their favourites */
 INSERT INTO UserFavouritePart 
-SELECT '164', p.partNo
+SELECT 164, p.partNo
 FROM Part as p
 WHERE (EXISTS
 	(SELECT bp.partNo, u.userId FROM BuildPart as bp, User as u, Build as b
 	WHERE bp.buildNo = b.buildNo AND u.userId = 164 and p.partNo = bp.partNo)
-    AND NOT EXISTS(SELECT partNo FROM UserFavouritePart as ufp WHERE userId = 164 AND ufp.partNo = p.partNo))
+    AND NOT EXISTS(SELECT partNo FROM UserFavouritePart as ufp WHERE userId = 164 AND ufp.partNo = p.partNo));

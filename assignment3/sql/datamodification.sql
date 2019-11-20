f518cb59d7c29d089cdf941466f340e69efa7df5
rlim23 <46493295+rlim23@users.noreply.github.com>
2019-11-20 16:46:04 -0500

    removed old query and added Data modification queries

---
+++ assignment3/sql/datamodification.sql
@@ -0,0 +1,22 @@
+/* Data Modification Queries */ 
+
+-- insert build parts for a build where the part is in a user favourite part and is in stock as a certain store (intersect) 
+INSERT INTO BuildPart
+SELECT 141, p.partNo 
+FROM Part as p
+WHERE p.partNo IN (SELECT ufp.partNo FROM UserFavouritePart as ufp WHERE ufp.userId = (SELECT b.userId FROM Build as b WHERE buildNo = 141))
+AND p.partNo IN (SELECT i.partNo FROM Inventory as i WHERE storeNo = (SELECT storeNo FROM Store WHERE storeName = 'Amazon.ca')); 
+
+-- update the price of all the latest inventories of a specific part (i.e. the part has been sold out everywhere) 
+UPDATE Inventory as i
+SET price = 0.00 
+WHERE i.partNo = 10000 AND
+ inventoryDate = (SELECT MAX(inventoryDate) FROM Inventory as i2 WHERE  i2.partNo = 10000);
+
+-- delete all inventories for a store that are more than 120 days old OR are out of stock (price is 0) 
+DELETE FROM Inventory 
+WHERE storeNo = 5 
+AND ((inventoryDate < (current_date() - INTERVAL 120 DAY))
+	OR (price = 0.00)) ; 
+
+ 
--- assignment3/sql/queries.sql
+++ assignment3/sql/queries.sql
@@ -40,28 +40,11 @@ select storeNo, storeName
 from inventory inner join store using (storeNo)
 where inventoryDate = '2019-12-01' and price > 0 and partNo = 10582;
 
-
-
-
 									-- Ruiqi
 -- ------------------------------------------------------------------------------------------------------------
 									-- Robyn
 
-/* Six Queries */ 
-
-/* Find a store that has the all of the parts for a certain build and the cheapest total price of those parts */
-select storeNo, sum(price) as total
-from 
-	(select storeNo, partNo, price
-	from inventory inner join (select * from buildpart inner join build using (buildNo) ) as parts using (partNo)
-	where inventory.inventoryDate = '2019-12-01' and price > 0 and buildNo = 3
-	order by storeNo, buildNo, partNo) as storePartprice
-group by storeNo
-having count(partNo) = 8
-order by total asc
-limit 1;
-
-/*  Return all of the latest inventory prices that are lower than the average price for a certain part with no. 10000*/ 
+/* sixth query: Return all of the latest inventory prices that are lower than the average price for a certain part with no. 10000*/ 
 SELECT storeNo, partNo, price 
 FROM Inventory as i1
 WHERE partNo = 10000 

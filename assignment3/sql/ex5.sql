b6f9b73d9979604ed955220b7fc2f71020318031
Ruiqi Tian <rtian25@uwo.ca>
2019-11-21 16:37:27 -0500

    fixed ex5 query 1

--- assignment3/sql/ex5.sql
+++ assignment3/sql/ex5.sql
@@ -1,21 +1,17 @@
 -- first query: find the name of the store that yields the lowest price for all the parts that are in a certain build
--- works
-select storeNo, storeName, total
-from store inner join 
-	(select storeNo, sum(price) as total
-	from 
-		(select storeNo, partNo, price
-		from inventory inner join 
-			(select * from buildpart inner join build using (buildNo) ) as parts using (partNo)
-		where inventory.inventoryDate = '2019-12-01' and price > 0 and buildNo = 1
-		order by storeNo, buildNo, partNo) as storePartprice
-	group by storeNo
-	order by total asc
-	limit 1) 
-as mininalPriceStore using (storeNo);
+select storeNo, storeName, sum(price)
+from inventory inner join store using (storeNo) inner join buildpart using (partNo)
+where storeNo in (select inv.storeNo
+		from inventory as inv
+		where inv.price > 0 and inv.inventoryDate = '2019-12-01' and inv.partNo in (select partNo from buildpart where buildNo = 2)
+		group by inv.storeNo
+		having count(inv.partNo) = (select count(partNo) from buildpart where buildNo = 2))
+and buildNo = 2 and inventoryDate = '2019-12-01'
+group by storeNo
+order by price desc
+limit 1;
 
 -- second query: find the most popular 10 parts (featured in most builds)
--- works
 select partNo, partName, count(partNo) as occurance
 from buildpart inner join part using (partNo)
 group by partNo
@@ -23,7 +19,6 @@ order by occurance desc
 limit 10;
 
 -- third query: find all the parts that currently have the lowest price among all historical prices
-
 select p.partNo, p.partName, s.storeNo, s.storeName, price
 from inventory as inv, store as s, part as p
 where inventoryDate = '2019-12-01' 
@@ -38,19 +33,16 @@ where inventoryDate = '2019-12-01'
 
 
 -- fourth query: show all builds and parts for a certain user
--- works
 select buildNo, buildName, partNo, partName
 from build inner join buildpart using (buildNo) inner join part using (partNo)
 where userId = 233;
 
 -- fifth query: find all stores that have a certain part in stock
--- works
 select storeNo, storeName
 from inventory inner join store using (storeNo)
 where inventoryDate = '2019-12-01' and price > 0 and partNo = 10582;
 
 -- sixth query: find users who has more than 40 favourite parts
--- works
 select userId, userNickname, count(partNo) as numFavPart
 from user left join userfavouritepart using (userId)
 group by userId

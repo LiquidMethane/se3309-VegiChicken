4c4b7f8de03e8905c2a182a7cc40814f696acfdb
rlim23 <46493295+rlim23@users.noreply.github.com>
2019-11-21 11:30:23 -0500

    data modification queries

--- assignment3/sql/datamodification.sql
+++ assignment3/sql/datamodification.sql
@@ -8,15 +8,16 @@ WHERE p.partNo IN (SELECT ufp.partNo FROM UserFavouritePart as ufp WHERE ufp.use
 AND p.partNo IN (SELECT i.partNo FROM Inventory as i WHERE storeNo = (SELECT storeNo FROM Store WHERE storeName = 'Amazon.ca')); 
 
 -- update the price of all the latest inventories of a specific part (i.e. the part has been sold out everywhere) 
-UPDATE Inventory as i
-SET price = 0.00 
-WHERE i.partNo = 10000 AND
- inventoryDate = (SELECT MAX(inventoryDate) FROM Inventory as i2 WHERE  i2.partNo = 10000);
+UPDATE Inventory
+SET price = FLOOR(price*0.75) + 0.99 
+WHERE partNo = 10000 AND
+ inventoryDate = '2019-12-01'; -- (SELECT MAX(inventoryDate) FROM Inventory as i2 WHERE  i2.partNo = 10000);
 
 -- delete all inventories for a store that are more than 120 days old OR are out of stock (price is 0) 
 DELETE FROM Inventory 
 WHERE storeNo = 5 
 AND ((inventoryDate < (current_date() - INTERVAL 120 DAY))
 	OR (price = 0.00)) ; 
+    
 
  
--- assignment3/sql/queries.sql
+++ assignment3/sql/queries.sql
@@ -7,10 +7,9 @@ from store inner join
 		(select storeNo, partNo, price
 		from inventory inner join 
 			(select * from buildpart inner join build using (buildNo) ) as parts using (partNo)
-		where inventory.inventoryDate = '2019-12-01' and price > 0 and buildNo = 233
+		where inventory.inventoryDate = '2019-12-01' and price > 0 and buildNo = 1
 		order by storeNo, buildNo, partNo) as storePartprice
 	group by storeNo
-	having count(partNo) = 8
 	order by total asc
 	limit 1) 
 as mininalPriceStore using (storeNo);

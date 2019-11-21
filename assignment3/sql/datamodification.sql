94641a71aa1288eee5f24e5a32ba8ec012be5889
rlim23 <46493295+rlim23@users.noreply.github.com>
2019-11-21 15:38:14 -0500

    change update query in datamodification

--- assignment3/sql/datamodification.sql
+++ assignment3/sql/datamodification.sql
@@ -7,16 +7,14 @@ FROM Part as p
 WHERE p.partNo IN (SELECT ufp.partNo FROM UserFavouritePart as ufp WHERE ufp.userId = (SELECT b.userId FROM Build as b WHERE buildNo = 141))
 AND p.partNo IN (SELECT i.partNo FROM Inventory as i WHERE storeNo = (SELECT storeNo FROM Store WHERE storeName = 'Amazon.ca')); 
 
--- update the price of all the latest inventories of a specific part (i.e. the part has been sold out everywhere) 
-UPDATE Inventory as i
-SET price = 0.00 
-WHERE i.partNo = 10000 AND
- inventoryDate = (SELECT MAX(inventoryDate) FROM Inventory as i2 WHERE  i2.partNo = 10000);
+-- update the price of all the latest inventories of a specific part to be discounted by 25% 
+UPDATE Inventory
+SET price = FLOOR(price * 0.75) + 0.99 -- Floor and add 99 so the price ends in 0.99 
+WHERE partNo = 10000 AND
+ inventoryDate = '2019-12-01';
 
 -- delete all inventories for a store that are more than 120 days old OR are out of stock (price is 0) 
 DELETE FROM Inventory 
 WHERE storeNo = 5 
 AND ((inventoryDate < (current_date() - INTERVAL 120 DAY))
 	OR (price = 0.00)) ; 
-
- 

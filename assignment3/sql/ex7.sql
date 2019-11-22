bc306e452907659c0b84c018430dd00124443b65
Ruiqi Tian <rtian25@uwo.ca>
2019-11-21 22:11:08 -0500

    added insert commands to views

--- assignment3/sql/ex7.sql
+++ assignment3/sql/ex7.sql
@@ -4,12 +4,21 @@ create view partsByPopularity
 	from userfavouritepart inner join part using (partNo)
 	group by partNo
 	order by occurance desc;
+    
+-- insert into parts by popularity
+insert into partsByPopularity values (10801, 'Intel Core i9-9999XSEKT', 190);
 
+    
     
 -- view 2: view of a user's general information (for userId 233 in this case)
 create view personalInfo (userId, nickname)
 	as select userId, userNickname
     from user;
+    
+-- insert into parts out of stock
+insert into partsOutOfStock values (5, 10801, 'Intel Core i9-9999XSEKT');
+
+
     
 -- view 3: view of all the parts that are out of stock at each store at the moment (2019-12-01)
 create view partsOutOfStock as
@@ -17,3 +26,6 @@ create view partsOutOfStock as
     from part inner join inventory using (partNo)
     where price = 0 and inventoryDate = '2019-12-01'
     order by storeNo, partNo;
+    
+-- insert into personalInfo
+insert into personalInfo values (1001, 'SlickyMeme');

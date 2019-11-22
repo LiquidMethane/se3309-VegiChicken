35befb673f12017a29a1f3a435f0c829d624cda3
rlim23 <46493295+rlim23@users.noreply.github.com>
2019-11-21 20:46:51 -0500

    fixed an insert favourite parts query

---
+++ assignment3/sql/ex3.sql
@@ -0,0 +1,22 @@
+/* INSERT commands for UserFavouritePart relation */ 
+
+/* Simple Insert - a specific user and part number*/ 
+INSERT INTO UserFavouritePart
+VALUES ('1', 10040);
+
+/* Insert any favourite parts for a user with id 1 that are Memory and where an inventory with a price less than $100 exists for that part  */
+INSERT INTO UserFavouritePart
+SELECT 1, m.partNo
+FROM pcMemory as m
+WHERE EXISTS(SELECT * FROM Inventory as i 
+			 WHERE m.partNo = i.partNo AND price > 0.00 AND price < 100.00) 
+AND NOT EXISTS(SELECT partNo FROM UserFavouritePart as ufp WHERE userId = 1 AND ufp.partNo = m.partNo);
+
+/* Insert Favourite parts for a specific user (with userId 164) that the user has used in any of their builds and aren't already in their favourites */
+INSERT INTO UserFavouritePart 
+SELECT DISTINCT 164, p.partNo
+FROM Part as p
+WHERE (EXISTS
+	(SELECT bp.partNo, b.userId FROM BuildPart as bp, Build as b
+	WHERE bp.buildNo = b.buildNo AND b.userId = 164 AND p.partNo = bp.partNo)
+    AND NOT EXISTS(SELECT partNo FROM UserFavouritePart as ufp WHERE userId = 164 AND ufp.partNo = p.partNo));

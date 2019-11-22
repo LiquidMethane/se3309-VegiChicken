f79af8418cfc31cad778c680df7e4e3fbfd68d18
Ruiqi Tian <rtian25@uwo.ca>
2019-11-21 21:35:55 -0500

    fixed the third insert statement

--- assignment3/sql/ex3.sql
+++ assignment3/sql/ex3.sql
@@ -1,22 +1,20 @@
 /* INSERT commands for UserFavouritePart relation */ 
 
 /* Simple Insert - a specific user and part number*/ 
-INSERT INTO UserFavouritePart
+INSERT INTO userfavouritepart
 VALUES ('1', 10040);
 
 /* Insert any favourite parts for a user with id 1 that are Memory and where an inventory with a price less than $100 exists for that part  */
-INSERT INTO UserFavouritePart
+INSERT INTO userfavouritepart
 SELECT 1, m.partNo
-FROM pcMemory as m
-WHERE EXISTS(SELECT * FROM Inventory as i 
+FROM pcmemory as m
+WHERE EXISTS(SELECT * FROM inventory as i 
 			 WHERE m.partNo = i.partNo AND price > 0.00 AND price < 100.00) 
-AND NOT EXISTS(SELECT partNo FROM UserFavouritePart as ufp WHERE userId = 1 AND ufp.partNo = m.partNo);
+AND NOT EXISTS(SELECT partNo FROM userfavouritepart as ufp WHERE userId = 1 AND ufp.partNo = m.partNo);
 
-/* Insert Favourite parts for a user that are in any of the user's builds and aren't already in their favourites */
-INSERT INTO UserFavouritePart 
-SELECT 164, p.partNo
-FROM Part as p
-WHERE (EXISTS
-	(SELECT bp.partNo, u.userId FROM BuildPart as bp, User as u, Build as b
-	WHERE bp.buildNo = b.buildNo AND u.userId = 164 and p.partNo = bp.partNo)
-    AND NOT EXISTS(SELECT partNo FROM UserFavouritePart as ufp WHERE userId = 164 AND ufp.partNo = p.partNo));
+-- insert parts in to userfavouritepart for a user if the part is in any of user's build and not alreadly in his or her fav part list
+insert into userfavouritepart
+select 232, partNo from (
+select distinct partNo
+from build inner join buildpart using (buildNo)
+where userId = 232 and partNo not in (select distinct partNo from userfavouritepart where userId = 232)) as parts;
